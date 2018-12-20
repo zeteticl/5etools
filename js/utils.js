db31a0a256a9ba78214a850329ff3d2e57b37869
hazmole <hazmole@users.noreply.github.com>
2018-12-20 12:35:12 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -2311,7 +2311,7 @@ Parser.ACTION_JSON_TO_FULL = {
 	"撤離": [
 		"If you take the Disengage action, your movement doesn't provoke opportunity attacks for the rest of the turn."
 	],
-	"迴避": [
+	"閃避": [
 		"When you take the Dodge action, you focus entirely on avoiding attacks. Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage. You lose this benefit if you are incapacitated (as explained in the appendix) or if your speed drops to 0."
 	],
 	"協助": [
