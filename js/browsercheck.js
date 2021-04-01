1ee86c75a0c21fa19fbbaad6520c5f1da07623a0
hmqgg <mitchx7@outlook.com>
2021-04-01 19:54:54 +0800

    Localize Browser Warning

--- js/browsercheck.js
+++ js/browsercheck.js
@@ -12,9 +12,9 @@ window.addEventListener("load", () => {
 		const $overlay = $(`<div class="flex-col flex-vh-center relative edge__overlay"/>`);
 		$btnClose.appendTo($overlay);
 		$overlay.append(`<div class="flex-col flex-vh-center">
-			<div class="edge__title mb-2">UPDATE YOUR BROWSER</div>
-			<div><i>It looks like you're using an outdated/unsupported browser.<br>
-			5etools recommends and supports the latest <a href="https://www.google.com/chrome/" class="edge__link">Chrome</a> and the latest <a href="https://www.mozilla.org/firefox/" class="edge__link">Firefox</a>.</i></div>
+			<div class="edge__title mb-2">更新你的浏览器</div>
+			<div><i>似乎你正在使用过时的/不支持的浏览器。<br>
+			5etools 推荐使用最新的 <a href="https://www.microsoft.com/zh-cn/edge/" class="edge__link">Edge Chromium</a> 和最新的 <a href="https://www.mozilla.org/firefox/" class="edge__link">Firefox</a>.</i></div>
 		</div>`).appendTo($body);
 	}
 });
