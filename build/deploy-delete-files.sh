cfa9d9c0864f781cc8b0306c4c7409f6abb6e6e5
hmqgg <mitchx7@outlook.com>
2021-09-13 19:09:31 +0800

    CI: clean more unnecessary files

--- build/deploy-delete-files.sh
+++ build/deploy-delete-files.sh
@@ -3,7 +3,10 @@
 echo "Delete unnecessary files for hosting 5etools..."
 
 rm *.md *.json *.yml || true
+rm .gitignore .gitmodules .eslintrc.js .stylelintrc.json .editorconfig || true
 rm -r test/ || true
 rm -r node/ || true
 rm -r node_modules/ || true
-rm .gitignore .eslintrc.js .editorconfig || true
+rm -r build/ || true
+rm -r .git || true
+rm -r .github || true
